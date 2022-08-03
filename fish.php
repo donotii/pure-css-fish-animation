1e15c4c652df00fb104cc4361826e297ef5cf25f
donotii <110573836+donotii@users.noreply.github.com>
2022-08-04 01:31:22 +0200

    Add files via upload

---
+++ fish.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>  
+<html>  
+     <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+     
+          <title>D O N O T I</title>
+          <link rel="icon" href="logow.png">
+     
+          <link rel="stylesheet" href="fish.css" />
+          <script src="script.js"></script>
+          <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+
+          <link rel="preconnect" href="https://fonts.googleapis.com">
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+          <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet">
+     </head>  
+     <body>
+
+          <div id="loading">
+               <div class="center">
+
+                    <div class="aquarium">
+                         <div class="fish">
+                              <div class="nageoire">
+                                   <div class="fin"></div>
+                                   <div class="fin fin-bottom"></div>
+                              </div>
+                              <div class="body">
+                                   <div class="eye">
+                                        <div class="pupil"></div>
+                                   </div>
+                              </div>
+                         </div>
+                         <div class="shadow"></div>
+                    </div>
+
+                    <h1>BOB The fish</h1>
+                    <div class="loading">
+                         <div class="loadingbar"></div>
+                    </div>
+               </div>
+          </div>
+
+     </body>
+</html>
