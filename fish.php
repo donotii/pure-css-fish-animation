f73400b9dab539c671e6117ac1f3016949867421
donotii <110573836+donotii@users.noreply.github.com>
2022-08-04 01:35:37 +0200

    Update fish.php

--- fish.php
+++ fish.php
@@ -1,19 +1,17 @@
-<div id="loading">
-     <div class="center">
-          <div class="aquarium">
-               <div class="fish">
-                    <div class="nageoire">
-                         <div class="fin"></div>
-                         <div class="fin fin-bottom"></div>
-                    </div>
-                    <div class="body">
-                         <div class="eye">
-                              <div class="pupil"></div>
-                         </div>
-                    </div>
-               </div>
-               <div class="shadow"></div>
-          </div>
-          <h1>BOB The fish</h1>
-     </div>
+ <div class="center">
+      <div class="aquarium">
+           <div class="fish">
+                <div class="nageoire">
+                     <div class="fin"></div>
+                     <div class="fin fin-bottom"></div>
+                </div>
+                <div class="body">
+                     <div class="eye">
+                          <div class="pupil"></div>
+                     </div>
+                </div>
+           </div>
+           <div class="shadow"></div>
+      </div>
+      <h1>BOB The fish</h1>
 </div>
